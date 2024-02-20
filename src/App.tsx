aab8b3a682032d044b8cc7f3ef544c45a6cfdbeb
longlt201203 <longlt201203@nexondv.com>
2024-02-20 17:55:34 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState<{ status: string, data: any }>({
+    status: '',
+    data: null
+  });
+
+  const data = {
+    username: '',
+    password: ''
+  };
+
+  
+
+  function handleLogin() {
+    axios
+      .post("http://localhost:3000/auth/login", data)
+      .then((data) => {
+        setResult({
+          status: 'success',
+          data: data
+        })
+      })
+      .catch(err => {
+
+      });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Login</h1>
+      <form>
+        <div className='form-group'>
+          <label htmlFor="username">Username</label>
+          <input type="text" name='username' id='username' onChange={(e) => {
+            data.username = e.target.value;
+          }} />
+        </div>
+        <div className='form-group'>
+          <label htmlFor="password">Password</label>
+          <input type="password" name='password' id='password' onChange={(e) => {
+            data.password = e.target.value;
+          }} />
+        </div>
+        <div className='form-group'>
+          <button type='button' onClick={() => handleLogin()}>Login</button>
+        </div>
+      </form>
     </>
   )
 }
