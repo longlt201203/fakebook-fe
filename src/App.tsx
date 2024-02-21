8520c530faf38c4f66e805bcfbd8aa74cafbc24e
longlt201203 <longlt201203@nexondv.com>
2024-02-21 15:25:35 +0700

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 
@@ -8,41 +8,54 @@ function App() {
     data: null
   });
 
-  const data = {
+  const loginData = {
     username: '',
     password: ''
   };
 
-  
+  async function handleLogin() {
+    const baseUrl = "http://localhost:3000";
 
-  function handleLogin() {
-    axios
-      .post("http://localhost:3000/auth/login", data)
-      .then((data) => {
-        setResult({
-          status: 'success',
-          data: data
-        })
-      })
-      .catch(err => {
-
+    try {
+      let res = await axios.post(`${baseUrl}/auth/login-with-username-and-password`, loginData);
+      res = await axios.get(`${baseUrl}/auth/profile`, {
+        headers: {
+          Authorization: `Bearer ${res.data.accessToken}`
+        }
       });
+      
+      setResult({
+        status: 'success',
+        data: res.data.username
+      });
+    } catch (err) {
+      const errData = {
+        status: 'error',
+        data: { message: 'unknow error' }
+      }
+
+      if (err instanceof AxiosError) {
+        errData.data.message = err.response?.data.message;
+      }
+      setResult(errData);
+    }
   }
 
   return (
     <>
       <h1>Login</h1>
+      {result.status == 'error' ? <p className='text-danger'>{result.data.message}</p> : result.status == 'success' ? <p className='text-success'>Successfully login as {result.data}</p> : ''}
       <form>
         <div className='form-group'>
           <label htmlFor="username">Username</label>
           <input type="text" name='username' id='username' onChange={(e) => {
-            data.username = e.target.value;
+            loginData.username = e.target.value;
           }} />
         </div>
         <div className='form-group'>
           <label htmlFor="password">Password</label>
           <input type="password" name='password' id='password' onChange={(e) => {
-            data.password = e.target.value;
+            loginData.password = e.target.value;
           }} />
         </div>
         <div className='form-group'>
